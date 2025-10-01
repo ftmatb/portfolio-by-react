32c27dd3c6758d833644b99856fd34a6fd120331
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-10-01 13:56:48 +0200

    start and make first components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Home } from "lucide-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

ffb4b6ddb9062ad74623730f01e1a6598e5cc19b
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-10-05 09:47:37 +0200

    theme toggle and background effect(stars and meteors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { Star } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
+      <ThemeToggle />
 
       {/* Background Effects */}
+      <StarBackground />
 
       {/* Navbar */}
 
